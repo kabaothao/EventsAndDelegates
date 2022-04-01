a09018078790d520fc362b69551311259fcf40b7
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-01 10:13:05 -0500

    updated codes and comments

--- EventsAndDelegates/Program.cs
+++ EventsAndDelegates/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EventsAndDelegates // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var video = new Video() { Title = "Video 1" };//first we instantiate the video here
+            var videoencoder = new VideoEncoder();
+
+            videoEncoder.Encode(video); //then we instantiate the video encoder and finally we call the encode method, passing the video object.
+        }
+    }
+}
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class VideoEncoder
+    {
+        // 1. Define a delegate
+        // 2. Define an event based on that delegate
+        // 3. Raise the event 
+
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args);
+        public event VideoEncodedEventHandler VideoEncoded;
+        //To raise an event we need a method that is responsible for that.
+
+
+
+
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video..."); //log a message to the console
+            Thread.Sleep(3000);
+
+            OnVideoEncoded();
+        }
+
+
+
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, EventArgs.Empty);
+        }
+    }
+}
